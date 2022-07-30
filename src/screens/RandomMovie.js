dcdecb5ba296303f8854e7d7ff46919255141a66
Hossam <hossamnasser938@gmail.com>
2022-07-31 00:13:54 +0200

    create dummy screens

---
+++ src/screens/RandomMovie.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const RandomMovieScreen = props => {
+    return (
+        <View>
+            <Text>Random Movie</Text>
+        </View>
+    );
+};
+
+export default RandomMovieScreen;
---
+++ src/screens/SelectHero.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const SelectHeroScreen = props => {
+    return (
+        <View>
+            <Text>Select Hero Screen</Text>
+        </View>
+    );
+};
+
+export default SelectHeroScreen;

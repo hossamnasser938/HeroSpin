7517ff6dc19ff1abe83253d1eef33461347e3845
Hossam <hossamnasser938@gmail.com>
2022-07-31 01:17:28 +0200

    rename Home screen

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import HomeScreen from './src/screens/Home';
+import PickRandomMovieScreen from './src/screens/PickRandomMovie';
 import RandomMovieScreen from './src/screens/RandomMovie';
 import ChooseSuperHeroScreen from './src/screens/ChooseSuperHero';
 
@@ -12,7 +12,7 @@ const ChooseSuperHeroStackNavigator = createNativeStackNavigator();
 
 const PickRandomMovieStack = () => (
   <PickRandomMovieStackNavigator.Navigator screenOptions={{header: () => null}}>
-    <PickRandomMovieStackNavigator.Screen name="Home" component={HomeScreen}/>
+    <PickRandomMovieStackNavigator.Screen name="PickRandomMovieScreen" component={PickRandomMovieScreen}/>
     <PickRandomMovieStackNavigator.Screen name="RandomMovieScreen" component={RandomMovieScreen}/>
   </PickRandomMovieStackNavigator.Navigator>
 );

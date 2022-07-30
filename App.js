cba67db17779ad5e1cd9c8660cae0f27b66b2054
Hossam <hossamnasser938@gmail.com>
2022-07-31 01:14:05 +0200

    use tabs for app main screen

--- App.js
+++ App.js
@@ -1,18 +1,36 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from './src/screens/Home';
 import RandomMovieScreen from './src/screens/RandomMovie';
+import ChooseSuperHeroScreen from './src/screens/ChooseSuperHero';
 
-const Stack = createNativeStackNavigator();
+const MainTabsNavigator = createBottomTabNavigator();
+const PickRandomMovieStackNavigator = createNativeStackNavigator();
+const ChooseSuperHeroStackNavigator = createNativeStackNavigator();
+
+const PickRandomMovieStack = () => (
+  <PickRandomMovieStackNavigator.Navigator screenOptions={{header: () => null}}>
+    <PickRandomMovieStackNavigator.Screen name="Home" component={HomeScreen}/>
+    <PickRandomMovieStackNavigator.Screen name="RandomMovieScreen" component={RandomMovieScreen}/>
+  </PickRandomMovieStackNavigator.Navigator>
+);
+
+const ChooseSuperHeroStack = () => (
+  <ChooseSuperHeroStackNavigator.Navigator>
+    <ChooseSuperHeroStackNavigator.Screen name="ChooseSuperHeroScreen" component={ChooseSuperHeroScreen}/>
+    <PickRandomMovieStackNavigator.Screen name="RandomMovieScreen" component={RandomMovieScreen}/>
+  </ChooseSuperHeroStackNavigator.Navigator>
+);
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="RandomMovie" component={RandomMovieScreen} />
-      </Stack.Navigator>
+      <MainTabsNavigator.Navigator screenOptions={{header: () => null}}>
+        <MainTabsNavigator.Screen name="PickRandomMovieStack" component={PickRandomMovieStack} options={{tabBarLabel: 'Pick', tabBarIcon: () => null}} />
+        <MainTabsNavigator.Screen name="ChooseSuperHeroStack" component={ChooseSuperHeroStack} options={{tabBarLabel: "Choose & Pick", tabBarIcon: () => null}}/>
+      </MainTabsNavigator.Navigator>
     </NavigationContainer>
   );
 }
---
+++ src/screens/ChooseSuperHero.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const ChooseSuperHeroScreen = props => {
+    return (
+        <View>
+            <Text>Choose SuperHero</Text>
+        </View>
+    );
+};
+
+export default ChooseSuperHeroScreen;
